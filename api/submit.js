d69aa53ceecf8fb657ead2a88f74fdac46d8c3ef
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-06-11 03:38:13 -0300

    add api

---
+++ api/submit.js
@@ -0,0 +1,24 @@
+
+import { Octokit } from '@octokit/rest'
+
+let TOKEN = process.env.TOKEN
+let REPO = process.env.REPO
+
+let [owner, repo] = REPO.split('/')
+
+module.exports = async (req, res) => {
+  let { url } = req.query
+
+  let octokit = new Octokit({ auth: TOKEN })
+  let response = await octokit.issues.create({
+    owner,
+    repo,
+    title: 'archive_request',
+    body: url
+  })
+
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  res.setHeader('Location', response['html_url'])
+  res.status(302)
+}
