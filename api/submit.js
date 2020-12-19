59907b368de597f9a11f69e75aff4077044e18b5
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-12-19 07:54:39 -0400

    prevent invalid submission

--- api/submit.js
+++ api/submit.js
@@ -6,19 +6,30 @@ let REPOSITORY = process.env.REPOSITORY
 
 let [owner, repo] = REPOSITORY.split('/')
 
+function checkSubmission(url) {
+  if (url.match(/(\s@|^@)/)) {
+    return false
+  }
+  return true
+}
+
 module.exports = async (req, res) => {
   let { url } = req.body
 
-  let octokit = new Octokit({ auth: TOKEN })
-  let response = await octokit.issues.create({
-    owner,
-    repo,
-    title: 'archive_request',
-    body: url
-  })
+  if (checkSubmission(url)) {
+    let octokit = new Octokit({ auth: TOKEN })
+    let response = await octokit.issues.create({
+      owner,
+      repo,
+      title: 'archive_request',
+      body: url
+    })
 
-  await new Promise(resolve => setTimeout(resolve, 1000))
+    await new Promise(resolve => setTimeout(resolve, 1000))
 
-  res.setHeader('Location', response.data.html_url)
-  res.status(302).send('')
+    res.setHeader('Location', response.data.html_url)
+    res.status(302).send('')
+  } else {
+    res.status(200).send('你所提交了并非链接的内容，如有疑问请前往duty-machine-form项目提交issue。')
+  }
 }
